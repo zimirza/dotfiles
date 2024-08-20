9ca0314328d5d87c6332b0c1b9e495359e13d2c0
Zishan Mirza <zishan.mirza@h-d-gmbh.de>
2024-08-20 22:03:58 +0200

    added emacs cfg

---
+++ emacs/init.el
@@ -0,0 +1,27 @@
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(set-face-attribute 'default nil :height 250)
+
+(setq backup-by-copying t)
+(setq backup-directory-alist `(("." . "~/.cache/emacs/backup")))
+(setq auto-save-file-name-transforms
+  `((".*" "~/.cache/emacs/auto-save-list" t)))
+(setq delete-old-versions t)
+(setq kept-new-versions 6)
+(setq kept-old-versions 2)
+(setq version-control t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
