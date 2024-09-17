d70773b8c612ab9a9af7eee7b288117c8f6a80ae
Зишан Мирза <zmirza@tutanota.de>
2024-09-17 16:54:11 +0200

    add package for git

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -51,12 +51,17 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(use-package magit
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil))
+ '(custom-enabled-themes nil)
+ '(package-selected-packages
+   '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
